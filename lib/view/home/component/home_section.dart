93a238a1fa08a6381034e2834ade35bda0b19277
Nababkhan007 <markohenry46@gmail.com>
2023-01-27 02:44:31 +0600

    Add logout functionality

--- lib/view/home/component/home_section.dart
+++ lib/view/home/component/home_section.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:triffy/common/const/dimen_const.dart';
 import 'package:triffy/view_model/home_view_model.dart';
 import 'package:triffy/common/widget/custom_app_bar.dart';
 
@@ -11,6 +12,17 @@ class HomeSection extends GetWidget<HomeViewModel> {
     return CustomAppBar(
       automaticallyImplyLeading: false,
       title: "Triffy",
+      actions: [
+        IconButton(
+          padding: const EdgeInsets.all(
+            paddingMedium,
+          ),
+          onPressed: () => controller.logout(),
+          icon: const Icon(
+            Icons.power_settings_new,
+          ),
+        )
+      ],
       body: Container(),
     );
   }
--- lib/view_model/home_view_model.dart
+++ lib/view_model/home_view_model.dart
@@ -1,6 +1,35 @@
 import 'package:get/get.dart';
+import 'package:triffy/route/app_route.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:triffy/common/const/util_const.dart';
+import 'package:triffy/common/const/color_const.dart';
+import 'package:triffy/common/helper/get_storage_helper.dart';
 
 class HomeViewModel extends GetxController {
+  String _uid = "";
+
   @override
-  void onInit() {}
+  void onInit() {
+    _getSharedPrefValue();
+    super.onInit();
+  }
+
+  void logout() async {
+    await FirebaseAuth.instance.signOut();
+    _clearUidSharedPrefValue();
+    showSnackBar(
+      "Success",
+      "Logout successful",
+      leftBarIndicatorColor: colorWarning,
+    );
+    _goToLoginView();
+  }
+
+  void _goToLoginView() => Get.offAllNamed(AppRoute.login);
+
+  void _clearUidSharedPrefValue() =>
+      GetStorageHelper.removeGetStorageKey(GetStorageHelper.uid);
+
+  void _getSharedPrefValue() async =>
+      _uid = await GetStorageHelper.readGetStorageValue(GetStorageHelper.uid);
 }
