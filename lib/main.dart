199114d2a9ed3e6dc0efaf25befc6a87d85f3efa
Nababkhan007 <markohenry46@gmail.com>
2023-01-27 01:13:12 +0600

    Set up firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:triffy/route/app_page.dart';
+import 'package:triffy/firebase_options.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:triffy/common/core/binding.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:triffy/view/welcome/welcome_view.dart';
 import 'package:triffy/common/helper/theme_helper.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await GetStorage.init();
   runApp(const Triffy());
 }
