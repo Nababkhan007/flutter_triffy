bd26c5fffdac3e1758fb433ce020d14a58cff6a1
Nababkhan007 <markohenry46@gmail.com>
2023-01-26 23:24:15 +0600

    Add get material app with initial binding, get pages, theme and initialize get storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,29 @@
+import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:triffy/route/app_page.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:triffy/common/core/binding.dart';
+import 'package:triffy/view/welcome/welcome_view.dart';
+import 'package:triffy/common/helper/theme_helper.dart';
 
-void main() => runApp(const Triffy());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
+  runApp(const Triffy());
+}
 
 class Triffy extends StatelessWidget {
   const Triffy({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Triffy",
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text(
-            "Triffy",
-          ),
-        ),
-      ),
+      initialBinding: Binding(),
+      initialRoute: AppPage.initial,
+      getPages: AppPage.routes,
+      theme: ThemeHelper.lightTheme(context),
+      home: const WelcomeView(),
     );
   }
 }
