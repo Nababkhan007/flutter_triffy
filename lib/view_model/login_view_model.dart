9b6d0e6edc6d355fa5bad00aa2bba45847f18e93
Nababkhan007 <markohenry46@gmail.com>
2023-01-27 02:19:41 +0600

    Add firebase login

--- lib/view_model/login_view_model.dart
+++ lib/view_model/login_view_model.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:triffy/route/app_route.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:triffy/common/const/util_const.dart';
 import 'package:triffy/common/const/color_const.dart';
 import 'package:triffy/common/helper/get_storage_helper.dart';
@@ -27,16 +28,54 @@ class LoginViewModel extends GetxController {
                     "Please enter password",
                     leftBarIndicatorColor: colorWarning,
                   )
-                : _login();
+                : _loginWithFirebase();
   }
 
-  void _login() {
-    _saveSharedPrefValue();
-    _goToHomePage();
+  Future _loginWithFirebase() async {
+    try {
+      UserCredential credential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+
+      showSnackBar(
+        "Success",
+        "Login successful",
+        leftBarIndicatorColor: colorSuccess,
+      );
+      _saveSharedPrefValue(credential.user!.uid);
+      _goToHomePage();
+    } on FirebaseAuthException catch (e) {
+      if (e.code == "invalid-email") {
+        showSnackBar(
+          "Warning",
+          "Invalid email.",
+          leftBarIndicatorColor: colorWarning,
+        );
+      } else if (e.code == "user-not-found") {
+        showSnackBar(
+          "Warning",
+          "No user found for that email.",
+          leftBarIndicatorColor: colorWarning,
+        );
+      } else if (e.code == "wrong-password") {
+        showSnackBar(
+          "Warning",
+          "Wrong password provided for that user.",
+          leftBarIndicatorColor: colorWarning,
+        );
+      } else {
+        showSnackBar(
+          "Warning",
+          "Sorry, something went wrong!",
+          leftBarIndicatorColor: colorWarning,
+        );
+      }
+    } catch (e) {
+      // ignore :)
+    }
   }
 
-  void _saveSharedPrefValue() =>
-      GetStorageHelper.writeGetStorageValue(GetStorageHelper.email, email);
+  void _saveSharedPrefValue(String uid) =>
+      GetStorageHelper.writeGetStorageValue(GetStorageHelper.uid, uid);
 
   void _goToHomePage() => Get.offAllNamed(AppRoute.home);
 
