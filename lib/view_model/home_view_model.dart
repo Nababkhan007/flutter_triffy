79a27766de158e718f20a638cb34c180e8d49ffa
Nababkhan007 <markohenry46@gmail.com>
2023-01-28 02:57:14 +0600

    Fetch trip data from cloud firestore

--- lib/view_model/home_view_model.dart
+++ lib/view_model/home_view_model.dart
@@ -1,17 +1,33 @@
+import 'dart:convert';
 import 'package:get/get.dart';
 import 'package:triffy/route/app_route.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:triffy/common/const/util_const.dart';
 import 'package:triffy/common/const/color_const.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:triffy/model/network/trip_model.dart';
 import 'package:triffy/common/helper/get_storage_helper.dart';
 
-class HomeViewModel extends GetxController {
+class HomeViewModel extends GetxController with StateMixin<TripModel> {
   String _uid = "";
+  Rx<TripModel> trip = TripModel().obs;
 
   @override
   void onInit() {
     _getSharedPrefValue();
+    _getTrips();
     super.onInit();
+  }
+
+  void _getTrips() async {
+    change(trip.value, status: RxStatus.loading());
+    FirebaseFirestore db = FirebaseFirestore.instance;
+    await db.collection("trip").get().then((event) {
+      for (var doc in event.docs) {
+        trip.value = tripModelFromJson(json.encode(doc.data()));
+      }
+    });
+    change(trip.value, status: RxStatus.success());
   }
 
   void logout() async {
