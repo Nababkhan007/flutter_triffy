1d984a99348ec0472c85afe6cd11424f3705776c
Nababkhan007 <markohenry46@gmail.com>
2023-01-29 01:24:43 +0600

    Add floating action button

--- lib/common/widget/custom_scaffold.dart
+++ lib/common/widget/custom_scaffold.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 
 class CustomScaffold extends StatelessWidget {
-  final Widget body;
+  final Widget body, fab;
+  final FloatingActionButtonLocation fabLocation;
 
   const CustomScaffold({
     Key? key,
     required this.body,
+    required this.fab,
+    this.fabLocation = FloatingActionButtonLocation.endFloat,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: body,
+      floatingActionButton: fab,
+      floatingActionButtonLocation: fabLocation,
     );
   }
 }
